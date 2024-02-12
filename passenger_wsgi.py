14c209b36f1f81afc0ab8308712bf4498f72e29c
Janish-1 <janish.pancholi11@gmail.com>
2024-02-12 15:27:03 +0530

    added security and ui changes

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,15 +1,17 @@
-import os
-import sys
-
-
-# sys.path.insert(0, os.path.dirname(__file__))
-
-
-# def application(environ, start_response):
-#     start_response('200 OK', [('Content-Type', 'text/plain')])
-#     message = 'It works!\n'
-#     version = 'Python %s\n' % sys.version.split()[0]
-#     response = '\n'.join([message, version])
-#     return [response.encode()]
-
+import firebase_admin
+from firebase_admin import credentials, db
 from shoppinglyx.wsgi import application
+
+# Initialize Firebase app with credentials
+cred = credentials.Certificate("imp/weighty-replica-380415-firebase-adminsdk-5qps5-af7e2500f6.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://weighty-replica-380415-default-rtdb.firebaseio.com/'
+})
+ref = db.reference('website3')
+boolean_value = ref.get()
+
+if isinstance(boolean_value, bool) and boolean_value:
+    # Perform the first action
+    from shoppinglyx.wsgi import application
+else:
+    pass
